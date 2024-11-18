3cbbdd40e17b2f243fb651abd03e1041db338345
Big+ <4bigchoi23@gmail.com>
2024-11-18 14:06:50 +0900

    refactor: server side rendering

--- src/pages/api/youtube.json.ts
+++ src/pages/api/youtube.json.ts
@@ -1,5 +1,7 @@
-// Outputs: /youtube.json
-export async function GET() {
+import type { APIContext } from 'astro';
+
+export const prerender = false;
+export async function GET(context: APIContext) {
   const response = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=48&regionCode=KR&key=${import.meta.env.GG_TOKEN}`);
   return new Response(await response.arrayBuffer());
 }
