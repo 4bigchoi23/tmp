e0590512cb2bc55e7ca3c8caf0542921dc141a2e
Big+ <4bigchoi23@gmail.com>
2024-11-18 14:22:58 +0900

    refactor: output static

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,9 +6,5 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
-  adapter: node({
-    mode: 'standalone',
-  }),
   integrations: [react()]
 });
